742f42cdfc0eb38b4b350d38e3cdb7e8cb1bc894
Erwin Bolwidt <ebolwidt@worldturner.nl>
2011-03-06 01:47:13 +0800

    Fix

--- sandbox/hlpartitiontable_unmap.rb
+++ sandbox/hlpartitiontable_unmap.rb
@@ -5,10 +5,18 @@ require 'hl-partition.rb'
 require 'file/file_patch.rb'
 require 'debootstrap/debootstrap.rb'
 
-#path = "tmp/highlevel.img"
-path = "/home/vmplanet/cabochon/tmp/highlevel.img"
-
 KernelExt::debug = true
+
+path = "tmp/highlevel.img"
+#path = "/home/vmplanet/cabochon/tmp/highlevel.img"
+mount_point = "tmp/mountpoint"
+
+bootstrap = Debootstrap.new
+bootstrap.root_path = mount_point
+bootstrap.apt_cache_path = "tmp/apt-cache"
+bootstrap.apt_lib_path = "tmp/apt-lib"
+
+bootstrap.unbind
 
 DevMapper.unmount_partitions(path)
 DevMapper.unmap_partitions_to_devices(path)
